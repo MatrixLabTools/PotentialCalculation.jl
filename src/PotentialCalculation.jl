c5fff15d5ce1102b5ca4e5d59f4e63d6165f2461
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2020-03-05 15:38:03 +0200

    Reorganized exporting

--- src/PotentialCalculation.jl
+++ src/PotentialCalculation.jl
@@ -1,99 +1,30 @@
 module PotentialCalculation
 
-export m_electron,
-       m_u,
-       m_au,
-       proton_mass,
-       masses,
-       AbstractAtom,
-       AbstractAtomWithMass,
-       AtomOnlySymbol,
-       AtomWithMass,
-       AbstractMolecule,
-       Molecule,
-       MoleculeIdenticalInformation,
-       makeidentical!,
-
-       AbstractCluster,
-       AbstractClusterWithSymbols,
-       Cluster,
-       cluster_angle,
-       ClusterNoSymbols,
-       ClusterWithMass,
-       dihedral_angle,
-       distances,
-       print_xyz,
-
-       AbstractIdentical,
-       Identical,
-       areidentical,
-
-       load_jld_data,
-       read_xyz,
-       save_jld_data,
-
-       AbstractCalculator,
-       AbstractCalculationProgram,
-       Orca,
-       Calculator,
-       calculate_energy,
-       bsse_corrected_energy,
-
-       change_energy_unit,
-       energy_from,
-       energy_to,
-
-       line_sampler,
-       adaptive_line_sampler,
-       sample_multiple_adaptive_lines,
-       InputAdaptiveSampler,
-
-
-       calculate_adaptive_sample_inputs,
-       calculate_energy_for_xyzfile,
-       calculate_potential,
-       calculate_with_different_method,
-       continue_calculation,
-       create_inputs,
-       load_clusters_and_make_input,
-       load_clusters_and_sample_input,
-       load_data_file,
-       load_restart_file,
-       test_work,
-       write_restart_file,
-       write_save_file
+using Reexport
 
 
 include("identical.jl")
+@reexport using .identical
 include("atoms.jl")
+@reexport using .atoms
 include("molecules.jl")
+@reexport using .molecules
 include("clusters.jl")
-
+@reexport using .clusters
 
 include("unitconversions.jl")
+@reexport using .unitconversions
 include("fileaccess.jl")
+@reexport using .fileaccess
 
 include("calculators.jl")
+@reexport using .calculators
 include("psi4.jl")
 include("sample.jl")
+@reexport using .sample
 
 include("restarttools.jl")
-
-
-
-
-using .atoms
-using .molecules
-using .clusters
-using .identical
-using .unitconversions
-using .fileaccess
-using .calculators
-using .sample
-using .restarttools
-
-
-
+@reexport using .restarttools
 
 
 end # module
