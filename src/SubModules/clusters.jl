a83c61acf16f2af5a350d432281868258d5890b6
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2023-03-07 01:07:03 +0200

    ExtXYZ conversions

--- src/SubModules/clusters.jl
+++ src/SubModules/clusters.jl
@@ -20,6 +20,7 @@ export rotate_randomly!
 using AtomsBase
 using ..Atoms
 using Distances: Euclidean, pairwise, euclidean
+using ExtXYZ
 using LinearAlgebra
 using Rotations
 using StaticArrays
@@ -360,5 +361,31 @@ function AtomsBase.FlexibleSystem(c::Cluster; kwargs...)
     return isolated_system(a; kwargs...)
 end
 
+## ExtXYZ conversion tools
+
+function ExtXYZ.write_dict(c::Cluster; kwargs...)
+    d = Dict{String,Any}()
+    arrays = Dict{String,Any}()
+    info = Dict{String,Any}( String(p.first) => p.second for p in kwargs )
+    if ! haskey(info, "comment")
+        info["comment"] = ""
+    end
+    arrays["pos"] = c.xyz
+    arrays["species"] = [ x.id for x in c.atoms ]
+    d["N_atoms"] = length(c)
+    d["arrays"] = arrays
+    d["info"] = info
+    return d
+end
+
+function Cluster(d::Dict{String, Any})
+    @assert haskey(d, "N_atoms")
+    @assert haskey(d, "arrays")
+
+    pos = d["arrays"]["pos"]
+    a = AtomOnlySymbol.(d["arrays"]["species"])
+    c = Cluster( pos, a )
+    return c
+end
 
 end #module Clusters
